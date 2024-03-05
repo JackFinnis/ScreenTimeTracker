814c1692971cf4905a4285cc2976715a7411bae8
Jack Finnis <jack.finnis@icloud.com>
2024-03-05 01:42:07 +0000

    Initial Commit

---
+++ ScreenTimeTracker/ScreenTimeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTimeTrackerApp.swift
+//  ScreenTimeTracker
+//
+//  Created by Jack Finnis on 05/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTimeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
