2a749fc708e8251002bb4186b889b5f727d9463d
Jack Finnis <jack.finnis@icloud.com>
2025-11-07 16:45:32 +0000

    .

--- ShieldHandler/ShieldHandler.swift
+++ ShieldHandler/ShieldHandler.swift
@@ -12,34 +12,38 @@ import FamilyControls
 import UIKit
 
 class ShieldHandler: ShieldActionDelegate {
-    func handle(action: ShieldAction, completionHandler: @escaping (ShieldActionResponse) -> Void) {
-        switch action {
-        case .secondaryButtonPressed:
-            let schedule = DeviceActivitySchedule(
-                intervalStart: Date.now.addingTimeInterval(-oneHour).timeComponents,
-                intervalEnd: Date.now.addingTimeInterval(60).timeComponents,
-                repeats: false
-            )
-            do {
-                try DeviceActivityCenter().startMonitoring(.snooze, during: schedule)
-            } catch {
-                print(error)
+    func handle(action: ShieldAction, completion: @escaping (ShieldActionResponse) -> Void) {
+        Task {
+            switch action {
+            case .secondaryButtonPressed:
+                try? await Task.sleep(for: .seconds(5))
+                
+                let schedule = DeviceActivitySchedule(
+                    intervalStart: Date.now.addingTimeInterval(-oneHour).timeComponents,
+                    intervalEnd: Date.now.addingTimeInterval(60).timeComponents,
+                    repeats: false
+                )
+                do {
+                    try DeviceActivityCenter().startMonitoring(.snooze, during: schedule)
+                } catch {
+                    print(error)
+                }
+                completion(.none)
+            default:
+                completion(.close)
             }
-            completionHandler(.none)
-        default:
-            completionHandler(.close)
         }
     }
     
     override func handle(action: ShieldAction, for application: ApplicationToken, completionHandler: @escaping (ShieldActionResponse) -> Void) {
-        handle(action: action, completionHandler: completionHandler)
+        handle(action: action, completion: completionHandler)
     }
     
     override func handle(action: ShieldAction, for category: ActivityCategoryToken, completionHandler: @escaping (ShieldActionResponse) -> Void) {
-        handle(action: action, completionHandler: completionHandler)
+        handle(action: action, completion: completionHandler)
     }
     
     override func handle(action: ShieldAction, for webDomain: WebDomainToken, completionHandler: @escaping (ShieldActionResponse) -> Void) {
-        handle(action: action, completionHandler: completionHandler)
+        handle(action: action, completion: completionHandler)
     }
 }
